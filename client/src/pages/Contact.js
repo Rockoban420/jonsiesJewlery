56298f2018c52dd17602ad135b19b4a64aa80f26
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-19 10:12:46 -0700

    implement front end contact page and add to navbar

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,31 @@
 import React from "react";
 
 const Contact = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Thank you for your message!");
+  };
+
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
