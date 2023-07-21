e4241cd910ee1d2ee2be346afa2bb166084470eb
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-07-20 23:44:17 -0700

    updated about me page

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -4,30 +4,23 @@ import kimImage from '../../pages/about/kim.jpg';
 
 const AboutMe = () => {
   return (
-    <Container maxWidth="md" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
-      <Card sx={{ maxWidth: 500, textAlign: 'center', backgroundColor: 'transparent', boxShadow: 'none' }}>
+    <Container maxWidth="md" sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+      <Card sx={{ maxWidth: 400, textAlign: 'left', backgroundColor: 'transparent', boxShadow: 'none', display: 'flex', alignItems: 'center', gap: '1rem' }}>
         <CardMedia
           component="img"
-          height="200"
           src={kimImage}
           alt="Kim Jones"
-          sx={{ borderRadius: '50%', margin: '20px auto 0', width: 250, height: 250, border: '2px solid white' }}
+          sx={{ width: '100%', height: '100%', maxHeight: '380px', objectFit: 'contain' }}
         />
+      </Card>
+
+      <Card sx={{ maxWidth: 400, textAlign: 'left', backgroundColor: 'transparent', boxShadow: 'none', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '1rem', }}>
         <CardContent>
-          <Typography variant="h4" sx={{ fontFamily: 'Ultra', color: '#674B3D' }} gutterBottom>
-            Hi, I'm Kim Jones!
+          <Typography variant="body1">
+          Kim Jones is a passionate jewelry creator based in the vibrant city of Portland, Oregon. Her love for exploring nature and the world around her has led her on a unique journey in jewelry making. She has a deep appreciation for ethically sourced materials, and she finds inspiration in the natural treasures she stumbles upon during her adventures.
           </Typography>
           <Typography variant="body1">
-            I'm a passionate jewelry creator based in the vibrant city of Portland, Oregon. My love for exploring nature
-            and the world around me has led me on a unique journey in jewelry making. I have a deep appreciation for
-            ethically sourced materials, and I find inspiration in the natural treasures I stumble upon during my
-            adventures.
-          </Typography>
-          <Typography variant="body1">
-            As an eco-conscious artist, I specialize in crafting one-of-a-kind jewelry pieces using ethically sourced
-            materials like bones, teeth, and even starfish. Each element I use has its own story, making every piece
-            truly unique and special. I believe in honoring nature's beauty by transforming these materials into
-            stunning and meaningful jewelry.
+          As an eco-conscious artist, Kim specializes in crafting one-of-a-kind jewelry pieces using ethically sourced materials like bones, teeth, and even starfish. Each element she uses has its own story, making every piece truly unique and special. Kim believes in honoring nature's beauty by transforming these materials into stunning and meaningful jewelry.
           </Typography>
         </CardContent>
       </Card>
